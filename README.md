a607490aecd5fba695ba19b224de66fe3782f110
Omar Jatoi <ouj@andrew.cmu.edu>
2015-01-16 21:28:13 -0500

    Changes and stuff

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# hill_cipher_solve
+# Hill Cipher Solver
+
+Basic Hill Cipher solver I wrote for Ghost in the Shellcode.
+
+To use, hardcode in the 3x3 matrix key in the python file. When it asks for the code, give it 3 letters of the ciphertext. The loop will run forever, so just keep typing your 3 letter chunks of the ciphertext till you're done and congrats! You have successfully decrypted a hill cipher.
+
+Look at http://crypto.interactive-maths.com/hill-cipher.html for description of how to encrypt and decrypt a hill cipher. There is a solver there, but it won't let you input a 3x3 matrix as the key.
--- hill_cipher.py
+++ hill_cipher.py
@@ -1,66 +1,28 @@
-def hill(code):
-    
+def hill(code): 
+    key = [[3,2,8],
+           [5,9,4],
+           [1,6,7]]
+
     code = code.lower()
-    output = [[0],
-              [0],
-              [0]]
+    output = [[0],[0],[0]]
     counter = 0
     for character in code:
         number = ord(character) - 97
         output[counter][0] = number
         counter += 1
-
-##    key = [[1,7,2],
-##           [6,4,5],
-##           [3,8,9]]
-
-##    key = [[5,4,9],
-##           [8,7,3],
-##           [2,6,1]]
-
-##    key = [[6,8,3],
-##           [2,1,9],
-##           [7,4,5]]
-##
-##    key = [[4,9,6],
-##           [8,1,3],
-##           [2,5,7]]
-####
-##    key = [[3,2,7],
-##           [4,5,6],
-##           [1,9,8]]
-##
-##    key = [[8,5,1],
-##           [9,7,2],
-##           [4,3,6]]
-##
-##    key = [[9,6,4],
-##           [7,3,1],
-##           [5,2,8]]
-##
-##    key = [[7,1,5],
-##           [6,8,2],
-##           [9,3,4]]
-##
-    key = [[3,2,8],
-           [5,9,4],
-           [1,6,7]]
     
     result = [[0],
              [0],
              [0]]
 
-    # iterate through rows of X
     for i in range(len(key)):
-       # iterate through columns of Y
        for j in range(len(output[0])):
-           # iterate through rows of Y
            for k in range(len(output)):
                result[i][0] += key[i][k] * output[k][j]
 
     for r in result:
-       a = r[0] % 26
-       val = chr(a + 97)
+       numeric_letter = r[0] % 26
+       val = chr(numeric_letter + 97)
        print(val)
 
 
