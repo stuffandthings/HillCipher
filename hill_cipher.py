d6db142d33e7139f4f4c51dc09e28974e39a6688
Omar Jatoi <ouj@andrew.cmu.edu>
2015-01-23 17:23:33 -0500

    Better code.

--- hill_cipher.py
+++ hill_cipher.py
@@ -1,8 +1,8 @@
 def hill(code): 
-    key = [[3,2,8],
-           [5,9,4],
-           [1,6,7]]
-
+    decryptionKey = [[2,0,3],
+                     [23,5,11],
+                     [7,6,25]]
+    
     code = code.lower()
     output = [[0],[0],[0]]
     counter = 0
@@ -10,22 +10,34 @@ def hill(code):
         number = ord(character) - 97
         output[counter][0] = number
         counter += 1
-    
+
     result = [[0],
              [0],
              [0]]
 
-    for i in range(len(key)):
+    for i in range(len(decryptionKey)):
        for j in range(len(output[0])):
            for k in range(len(output)):
-               result[i][0] += key[i][k] * output[k][j]
+               result[i][0] += decryptionKey[i][k] * output[k][j]
 
+    unCiphered = ""
     for r in result:
        numeric_letter = r[0] % 26
        val = chr(numeric_letter + 97)
-       print(val)
+       unCiphered = unCiphered + val
 
+    return unCiphered
 
-while(True):
-    code = raw_input("Enter Code: ")
-    hill(code)
+def main():
+    code = raw_input("Enter ciphertext: ")
+    print
+    plaintext = ""
+
+    while(code):
+        ciphertext = code[:3]
+        code = code[3:]
+        plaintext = plaintext + hill(ciphertext)
+        print plaintext
+        print
+                    
+main()
